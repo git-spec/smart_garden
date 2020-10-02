bf9bd65f52f17008eaec715d1fc4368807da459e
felixwurst <felix.wurst@gmail.com>
2020-10-02 13:07:22 +0200

    added route for products

--- public/src/components/Router.js
+++ public/src/components/Router.js
@@ -1,12 +1,12 @@
 /* **************************************************** MODULES *********************************************************** */
 import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import {connect} from 'react-redux'
+// import {connect} from 'react-redux'
 /* **************************************************** COMPONENTS *********************************************************** */
 import Main from './Main'
-import Register from './Register'
-import Login from './Login'
-import User from './User'
+// import Register from './Register'
+// import Login from './Login'
+// import User from './User'
 import Products from './Products'
 /* **************************************************** ROUTES *********************************************************** */
 function Router() {
@@ -14,13 +14,14 @@ function Router() {
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component={Main} />
-                <Route path='/register' exact component={Register} />
+                {/* <Route path='/register' exact component={Register} />
                 <Route path='/login' exact component={Login} />
-                <Route path='/user' exact component={User} />
+                <Route path='/user' exact component={User} /> */}
                 <Route path='/products' exact component={Products} />
             </Switch>
         </BrowserRouter>
     )
 }
 
-export default connect()(Router)
+// export default connect()(Router)
+export default Router
