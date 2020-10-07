f257cf0109115c9d98b86e5301eaadfd7f64a400
felixwurst <felix.wurst@gmail.com>
2020-10-07 10:38:38 +0200

    added check add get for hubs and devices

--- modules/devicesAuth.js
+++ modules/devicesAuth.js
@@ -1,17 +1,15 @@
-/* ************************************************************ SETUP ******************************************************* */
+/* ---------------------------------------- SETUP ---------------------------------------- */
 // modules
 const runQuery = require('./mysqlCon');
 
-/* ************************************************************ FUNCTIONS ******************************************************* */
-// check Hub Number
-function checkHubNum(num) {
+/* ---------------------------------------- FUNCTIONS ---------------------------------------- */
+function checkHubNum(hubNum) {
     return new Promise((resolve, reject) => {
-        runQuery(`SELECT * FROM iot_hubs WHERE sn_number LIKE '${num}'`).then(hits => {
-            // console.log('devicesAuth: ', hits);
-            if (hits.length === 0) {
+        runQuery(`SELECT * FROM iot_hubs WHERE sn_number LIKE '${hubNum}' AND user_id IS NULL`).then(data => {
+            if (data.length === 0) {
                 reject("not found");
             } else {
-                resolve(hits[0]);
+                resolve(data[0]);
             }
         }).catch(err => {
             console.log(err);
@@ -20,7 +18,81 @@ function checkHubNum(num) {
     });
 }
 
-/* ************************************************************ EXPORT ******************************************************* */
+function checkDeviceNum(deviceNum) {
+    return new Promise((resolve, reject) => {
+        runQuery(`SELECT * FROM iot_device WHERE sn_number LIKE '${deviceNum}' AND user_id IS NULL`).then(data => {
+            if (data.length === 0) {
+                reject("not found");
+            } else {
+                resolve(data[0]);
+            }
+        }).catch(err => {
+            console.log(err);
+            reject(err);
+        });
+    });
+}
+
+function addHub(hubNum) {
+    return new Promise((resolve, reject) => {
+        runQuery(`UPDATE iot_hubs SET user_id=1 WHERE sn_number='${hubNum}'`).then(data => {            
+            getHubs().then(data => {
+                resolve(data);
+            }).catch(error => {
+                reject(error);
+            });
+        }).catch(err => {
+            console.log(err);
+            reject(err);
+        });
+    });
+}
+
+function addDevice(hubID, deviceNum) {
+    return new Promise((resolve, reject) => {
+        runQuery(`UPDATE iot_device SET user_id=1, hub_id=${hubID} WHERE sn_number='${deviceNum}'`).then(data => {
+            // console.log('devicesAuth: ', data);
+            // resolve();
+            getDevices().then(data => {
+                resolve(data);
+            }).catch(error => {
+                reject(error);
+            });
+        }).catch(err => {
+            console.log(err);
+            reject(err);
+        });
+    });
+}
+
+function getHubs() {
+    return new Promise((resolve, reject) => {
+        runQuery('SELECT * FROM iot_hubs WHERE user_id=1').then(data => {            
+            resolve(data);
+        }).catch(err => {
+            console.log(err);
+            reject(err);
+        });
+    });
+}
+
+function getDevices() {
+    return new Promise((resolve, reject) => {
+        runQuery('SELECT * FROM iot_device WHERE user_id=1').then(data => {            
+            resolve(data);
+        }).catch(err => {
+            console.log(err);
+            reject(err);
+        });
+    });
+}
+
+/* ---------------------------------------- EXPORT ---------------------------------------- */
 module.exports = {
-    checkHubNum
+    checkHubNum,
+    checkDeviceNum,
+    addHub,
+    addDevice,
+    getHubs,
+    getDevices
 };
