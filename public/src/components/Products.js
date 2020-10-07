24941453a9a0aa12be7af6e87ea210a2e03ff15c
felixwurst <felix.wurst@gmail.com>
2020-10-07 10:39:41 +0200

    added device registration

--- public/src/components/Products.js
+++ public/src/components/Products.js
@@ -1,43 +1,80 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
     InputGroup, 
     InputGroupAddon, 
     Button, 
     Input, 
-    Label, 
-    // Col, 
-    // Row, 
+    Label,
     Container
 } from 'reactstrap';
-import {checkHubNumPost} from '../services/api';
+// components
+import AddDeviceModal from './AddDeviceModal'
+// services
+import {
+    checkHubNumPost, 
+    checkDeviceNumPost, 
+    addHubPost,
+    addDevicePost,
+    getHubsPost,
+    getDevicesPost
+} from '../services/devicesApi';
 
 const Products = props => {
     const initialState = {
-        hubID: '',
-        deviceID: '',
-        hubListContent: null,
-        deviceListContent: null,
+        hubNum: '',
+        hubs: [],
+        devices: [],
+        modalShow: false,
+        modalPayload: null
     };
     const [state, setState] = useState(initialState);
 
-    // console.log(state);
+    // run at initial render
+    useEffect(() => {
+        getHubsPost().then(hubs => {
+            // console.log(data)
+            if (hubs !== 2) {
+                getDevicesPost().then(devices => {
+                    if (devices !== 2) {
+                        setState({...state, hubs, devices});
+                    } else {
+                        alert('Server error!');
+                    }
+                }).catch(err => {
+                    console.log(err);
+                });
+            } else {
+                alert('Server error!');
+            }
+        }).catch(err => {
+            console.log(err);
+        });
+    }, []);
 
     const onAddHubBtnClick = e => {
         e.preventDefault();
-        checkHubNumPost(state.hubID.trim()).then(data => {
-            // console.log('Products.js: ', data);
+        checkHubNumPost(state.hubNum.trim()).then(data => {
             // 1 serialnumber found
-            // 2 serialnumber not found
+            // 2 serialnumber not found or already registered
             // 3 server error
             switch (data) {
                 case 1:
-                    alert('serialnumber found');
+                    addHubPost(state.hubNum.trim()).then(data => {
+                        // console.log('Products.js: ', data);
+                        if (data !== 2) {
+                            setState({...state, hubs: data});
+                        } else {
+                            alert('Server error!');
+                        }
+                    }).catch(err => {
+                        console.log(err);
+                    })
                     break;
                 case 2:
-                    alert('serialnumber not found');
+                    alert('Serialnumber not found or already registered!');
                     break;
                 case 3:
-                    alert('server error');
+                    alert('Server error!');
                     break;
                 default:
                     break;
@@ -45,90 +82,114 @@ const Products = props => {
         }).catch(err => {
             console.log(err);
         });
+    };
 
-        const hubList = (
-            <React.Fragment>
-                {state.hubListContent}
-                <li className="mb-3">
-                    Smart Garden Hub {state.hubID}
-                    <Button className="ml-2" outline color="danger" size="sm">
-                        Delete
-                    </Button>{' '}
-                    {/* <ul>{state.deviceListContent}</ul> */}
-                </li>
-            </React.Fragment>
-        );
+    const onAddDeviceBtnClick = (e, hubID) => {
+        e.preventDefault();
         setState({
             ...state,
-            hubListContent: hubList,
+            modalShow: true,
+            modalPayload: hubID
         });
     };
 
-    // const onAddDeviceBtnClick = e => {
-    //     e.preventDefault();
-    //     const deviceList = (
-    //         <React.Fragment>
-    //             {state.hubListContent}
-    //             <li className="mb-3">
-    //                 Smart Garden Device {state.deviceID}
-    //                 <Button className="ml-2" outline color="danger" size="sm">
-    //                     Delete
-    //                 </Button>{' '}
-    //             </li>
-    //         </React.Fragment>
-    //     );
-    //     setState({
-    //         ...state,
-    //         hubListContent: deviceList,
-    //     });
-    // };
+    const confirmAdding = (hubID, deviceNum) => {
+        checkDeviceNumPost(deviceNum.trim()).then(data => {
+            // 1 serialnumber found
+            // 2 serialnumber not found or already registered
+            // 3 server error
+            switch (data) {
+                case 1:
+                    addDevicePost(hubID, deviceNum.trim()).then(data => {
+                        console.log('Products.js: ', data);
+                        if (data !== 2) {
+                            setState({
+                                ...state, 
+                                devices: data, 
+                                modalShow: false
+                            });
+                        } else {
+                            alert('Server error!');
+                        }
+                    }).catch(err => {
+                        console.log(err);
+                    })
+                    break;
+                case 2:
+                    alert('Serialnumber not found or already registered!');
+                    break;
+                case 3:
+                    alert('Server error!');
+                    break;
+                default:
+                    break;
+            }
+        }).catch(err => {
+            console.log(err);
+        });
+    };
+
+    const hubsElement = state.hubs.map(hub => {
+        return (
+            <div key={hub.id} className="mb-3">
+                hub: {hub.id} 
+                <Button className="ml-2" outline color="danger" size="sm">
+                    Delete
+                </Button>{' '}
+                <Button
+                    className="ml-2" outline color="primary" size="sm"
+                    onClick={e => onAddDeviceBtnClick(e, hub.id)}
+                >Add Device
+                </Button>{' '}
+            </div>
+        );
+    });
+
+    const devicesElement = state.devices.map(device => {
+        return (
+            <div key={device.id} className="mb-3">
+                device: {device.id} | type: {device.type_id} | connected to hub: {device.hub_id}
+                <Button className="ml-2" outline color="danger" size="sm">
+                    Delete
+                </Button>{' '}
+                <Button
+                    className="ml-2" outline color="primary" size="sm"
+                    // onClick={e => onAddDeviceBtnClick(e, hub.id)}
+                >Edit
+                </Button>{' '}
+            </div>
+        );
+    });
 
     return (
         <Container>
+            <AddDeviceModal
+                className="bg-primary"
+                title="Add Smart Garden Device"
+                show={state.modalShow}
+                payload={state.modalPayload}
+                addDevice={confirmAdding}
+                close={() => setState({ ...state, modalShow: false })}
+            >
+            </AddDeviceModal>
             <h1>My Smart Garden</h1>
-            {/* <Row>
-            <Col> */}
             <InputGroup className="mb-3">
-                <Label for="hubInp" className="mr-1">
-                    Add a Smart Hub
-                </Label>
+                <Label for="hubInp" className="mr-1">Add a Smart Hub</Label>
                 <Input
                     id="hubInp"
-                    placeholder="Insert a Product ID"
+                    placeholder="Insert a Serialnumber"
                     onChange={e => setState({
                         ...state,
-                        hubID: e.target.value
+                        hubNum: e.target.value
                     })}
-                    value={state.hubID}
+                    value={state.hubNum}
                 />
                 <InputGroupAddon addonType="append">
-                    <Button color="secondary" onClick={onAddHubBtnClick}>
-                        Add
-                    </Button>{' '}
+                    <Button color="secondary" onClick={onAddHubBtnClick}>Add</Button>{' '}
                 </InputGroupAddon>
             </InputGroup>
-            {/* </Col>
-            </Row> */}
-            {/* <InputGroup className="mb-3">
-                <Label for="deviceInp" className="mr-1">
-                    Add a Smart Device
-                </Label>
-                <Input
-                    id="deviceInp"
-                    placeholder="Insert a Product ID"
-                    onChange={e => setState({
-                        ...state,
-                        deviceID: e.target.value,
-                    })}
-                    value={state.deviceID}
-                />
-                <InputGroupAddon addonType="append">
-                    <Button color="secondary" onClick={onAddDeviceBtnClick}>
-                        Add
-                    </Button>{' '}
-                </InputGroupAddon>
-            </InputGroup> */}
-            <ul>{state.hubListContent}</ul>
+            {hubsElement}
+            {devicesElement}
         </Container>
     );
 };
