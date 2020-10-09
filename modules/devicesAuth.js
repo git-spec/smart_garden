c26d05cc40e9bb17defd03d15567b16913b9445d
felixwurst <felix.wurst@gmail.com>
2020-10-09 12:16:07 +0200

    add getDevices deleteHub deleteDevice

--- modules/devicesAuth.js
+++ modules/devicesAuth.js
@@ -5,9 +5,12 @@ const runQuery = require('./mysqlCon');
 /* ---------------------------------------- FUNCTIONS ---------------------------------------- */
 function checkHubNum(hubNum) {
     return new Promise((resolve, reject) => {
-        runQuery(`SELECT * FROM iot_hubs WHERE sn_number LIKE '${hubNum}' AND user_id IS NULL`).then(data => {
+        // runQuery(`SELECT * FROM iot_hubs WHERE sn_number LIKE '${hubNum}' AND user_id IS NULL`).then(data => {
+        runQuery(`SELECT * FROM iot_hubs WHERE sn_number LIKE '${hubNum}'`).then(data => {
             if (data.length === 0) {
                 reject("not found");
+            } else if (data[0].user_id !== null) {
+                reject("already registered");
             } else {
                 resolve(data[0]);
             }
@@ -20,9 +23,12 @@ function checkHubNum(hubNum) {
 
 function checkDeviceNum(deviceNum) {
     return new Promise((resolve, reject) => {
-        runQuery(`SELECT * FROM iot_device WHERE sn_number LIKE '${deviceNum}' AND user_id IS NULL`).then(data => {
+        // runQuery(`SELECT * FROM iot_device WHERE sn_number LIKE '${deviceNum}' AND user_id IS NULL`).then(data => {
+        runQuery(`SELECT * FROM iot_device WHERE sn_number LIKE '${deviceNum}'`).then(data => {
             if (data.length === 0) {
                 reject("not found");
+            } else if (data[0].user_id !== null) {
+                reject("already registered");
             } else {
                 resolve(data[0]);
             }
@@ -38,8 +44,9 @@ function addHub(hubNum) {
         runQuery(`UPDATE iot_hubs SET user_id=1 WHERE sn_number='${hubNum}'`).then(data => {            
             getHubs().then(data => {
                 resolve(data);
-            }).catch(error => {
-                reject(error);
+            }).catch(err => {
+                console.log(err);
+                reject(err);
             });
         }).catch(err => {
             console.log(err);
@@ -48,15 +55,14 @@ function addHub(hubNum) {
     });
 }
 
-function addDevice(hubID, deviceNum) {
+function addDevice(deviceNum, hubID) {
     return new Promise((resolve, reject) => {
         runQuery(`UPDATE iot_device SET user_id=1, hub_id=${hubID} WHERE sn_number='${deviceNum}'`).then(data => {
-            // console.log('devicesAuth: ', data);
-            // resolve();
-            getDevices().then(data => {
+            getDevices(hubID).then(data => {
                 resolve(data);
-            }).catch(error => {
-                reject(error);
+            }).catch(err => {
+                console.log(err);
+                reject(err);
             });
         }).catch(err => {
             console.log(err);
@@ -76,10 +82,60 @@ function getHubs() {
     });
 }
 
-function getDevices() {
+function getDevices(hubID) {
     return new Promise((resolve, reject) => {
-        runQuery('SELECT * FROM iot_device WHERE user_id=1').then(data => {            
+        runQuery(`SELECT iot_device.*, device_types.name FROM iot_device 
+            INNER JOIN device_types ON iot_device.type_id=device_types.id 
+            WHERE user_id=1 AND hub_id=${hubID}`).then(data => {            
             resolve(data);
+        }).catch(err => {
+            console.log(err);
+            reject(err);
+        });
+    });
+}
+
+function deleteHub(hubID) {
+    return new Promise((resolve, reject) => {
+        runQuery(`UPDATE iot_hubs SET user_id=null WHERE id='${hubID}'`).then(data => {
+            deleteDevices(hubID).then(data => {
+                getHubs().then(data => {
+                    resolve(data);
+                }).catch(err => {
+                    console.log(err);
+                    reject(err);
+                });
+            }).catch(err => {
+                console.log(err);
+                reject(err);
+            });
+        }).catch(err => {
+            console.log(err);
+            reject(err);
+        });
+    });
+}
+
+function deleteDevices(hubID) {
+    return new Promise((resolve, reject) => {
+        runQuery(`UPDATE iot_device SET user_id=null, hub_id=null WHERE hub_id='${hubID}'`).then(data => {
+            resolve(data);
+        }).catch(err => {
+            console.log(err);
+            reject(err);
+        });
+    });
+}
+
+function deleteDevice(deviceID, hubID) {
+    return new Promise((resolve, reject) => {
+        runQuery(`UPDATE iot_device SET user_id=null, hub_id=null WHERE id='${deviceID}'`).then(data => {            
+            getDevices(hubID).then(data => {
+                resolve(data);
+            }).catch(err => {
+                console.log(err);
+                reject(err);
+            });
         }).catch(err => {
             console.log(err);
             reject(err);
@@ -94,5 +150,7 @@ module.exports = {
     addHub,
     addDevice,
     getHubs,
-    getDevices
+    getDevices,
+    deleteHub,
+    deleteDevice
 };
