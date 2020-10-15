cebffd79975bbb13d3bbc63cc6515124dbcc91e6
hamoudshwiri <hamoudshwiri>
2020-10-15 14:30:13 +0200

    mail verification

--- public/src/components/Verification.js
+++ public/src/components/Verification.js
@@ -1,35 +1,39 @@
-import React, {useEffect, useState} from 'react'
-import {useParams, Link, useHistory} from 'react-router-dom'
+import React, {useEffect, useState} from 'react';
+import {useParams, Link, useHistory} from 'react-router-dom';
 
-import {sendParams} from '../services/api'
+import {sendParams} from '../services/api';
 
 const Verification = () => {
-  const params = useParams()
-  const history = useHistory()
+    const params = useParams();
+    const history = useHistory();
 
-  useEffect(() => {
-    sendParams(params.email).then(data => {
-        console.log(data);
-        if (data !== 2) {
-            history.push('/login')
-        } else {
-            history.push('/register')
-        }
-    }).catch(err=>{
-        history.push('/register')
-    })
-  }, [])
-  
+    useEffect(() => {
+        sendParams(params.email)
+            .then(data => {
+                console.log(data);
+                if (data !== 2) {
+                    history.push('/login');
+                } else {
+                    history.push('/register');
+                }
+            })
+            .catch(err => {
+                history.push('/register');
+            });
+    }, []);
+
     return (
-      <React.Fragment>
-        <div className="breadcrumb">
-            <p>Thanks For Your Register</p>
-          <div className="container">
-            <Link className="breadcrumb-item" to="/login">Login</Link>
-          </div>
-        </div>
-      </React.Fragment>
-    )
-}
+        <React.Fragment>
+            <div className="breadcrumb">
+                <p>Thanks For Your Register</p>
+                <div className="container">
+                    <Link className="breadcrumb-item" to="/login">
+                        Login
+                    </Link>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+};
 
-export default Verification
+export default Verification;
