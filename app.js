9212b1a8a18bd992475b0e862af35d37b4c6a4be
felixwurst <felix.wurst@gmail.com>
2020-10-21 01:18:52 +0200

    formatted

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-const socket = require("socket.io-client")("http://felix.local:5000");
-const {log} = require("console")
-
+const socket = require('socket.io-client')('http://felix.local:5000');
+const {log} = require('console');
 
 const device_info = {
-    sn_number: "aNLWTgH&$^k_L2jN"
-}
+    sn_number: 'aNLWTgH&$^k_L2jN'
+};
 
+socket.on('connect', () => {
+    log('connected');
+    socket.emit('device_connect', device_info);
+});
 
-
-
-socket.on("connect", () => {
-    log("connected")
-    socket.emit("device_connect", device_info);
-})
-
-
-socket.on("toDevice", message=>{
-    log(message)
-})
+socket.on('toDevice', message => {
+    log(message);
+});
