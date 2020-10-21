52e5679bdca39ba06a0b4d6c2a1e3de176a6fcf9
hamoudshwiri <hamoudshwiri>
2020-10-21 15:49:31 +0200

    design merge

--- public/src/components/Login.js
+++ public/src/components/Login.js
@@ -1,10 +1,5 @@
 import React, {useState} from 'react';
-import {
-  InputGroup,
-  Button,
-  Input,
-  Container
-} from 'reactstrap';
+import {Container, Row, Col, Form, FormGroup, Label, Input, Button} from 'reactstrap';
 import {Link, useHistory} from 'react-router-dom';
 // import {connect} from 'react-redux'
 
@@ -12,150 +7,169 @@ import PopUpModal from './PopUpModal';
 import {loginPost} from '../services/api';
 // import {setUserAction} from '../actions'
 
-const Login = (props) => {
+const Login = props => {
+    // useEffect(() => {
+    //   props.setUserAction(null)
 
+    // }, []);
 
-  // useEffect(() => {
-  //   props.setUserAction(null)
-    
-  // }, []);
+    const history = useHistory();
+    const initialState = {
+        email: '',
+        password: '',
+        entriesError: false,
+        errorElement: null,
+        errorTitle: ''
+    };
+    const [myState, setMyState] = useState(initialState);
 
-
-  const history = useHistory()
-  const initialState = {
-    email: '',
-    password: '',
-    entriesError: false,
-    errorElement: null,
-    errorTitle: ''
-  }
-  const [myState, setMyState] = useState(initialState);
-
-  const onLoginBtnClick = (e) => {
-    e.preventDefault()
-    if (myState.email.trim() === '' || myState.password === '') {
-      const errorElement = (
-        <ul>
-          {myState
-            .email
-            .trim() === ''
-            ? <li>Email should not be empty</li>
-            : null}
-          {myState.password === ''
-            ? <li>Password should not be empty</li>
-            : null}
-        </ul>
-      )
-      setMyState({
-        ...myState,
-        entriesError: true,
-        errorElement,
-        errorTitle: 'Entries Error'
-      })
-    } else {
-      loginPost(myState.email, myState.password).then(data => {
-        switch (data) {
-          case 2:
-            setMyState({...myState, entriesError: true, errorElement: <p>there was a server error</p>, errorTitle: 'Server Error' });
-            break;
-          case 3:
-            setMyState({...myState, entriesError: true, errorElement: <p>Password is wrong</p>, errorTitle: 'Wrong password'});
-            break;
-          case 4:
-            setMyState({...myState, entriesError: true, errorElement: <p>the email that you used is not exist</p>, errorTitle: 'Email not exist' });
-            break;
-          case 1:
-            // show admin panel
-            // props.setUserAction(myState.email)
-            history.push('/user')
-            // console.log('ok ok ok');
-            break;
-          default:
-            break;
+    const onLoginBtnClick = e => {
+        e.preventDefault();
+        if (myState.email.trim() === '' || myState.password === '') {
+            const errorElement = (
+                <ul>
+                    {myState.email.trim() === '' ? <li>Email should not be empty</li> : null}
+                    {myState.password === '' ? <li>Password should not be empty</li> : null}
+                </ul>
+            );
+            setMyState({
+                ...myState,
+                entriesError: true,
+                errorElement,
+                errorTitle: 'Entries Error'
+            });
+        } else {
+            loginPost(myState.email, myState.password)
+                .then(data => {
+                    switch (data) {
+                        case 2:
+                            setMyState({
+                                ...myState,
+                                entriesError: true,
+                                errorElement: <p>there was a server error</p>,
+                                errorTitle: 'Server Error'
+                            });
+                            break;
+                        case 3:
+                            setMyState({
+                                ...myState,
+                                entriesError: true,
+                                errorElement: <p>Password is wrong</p>,
+                                errorTitle: 'Wrong password'
+                            });
+                            break;
+                        case 4:
+                            setMyState({
+                                ...myState,
+                                entriesError: true,
+                                errorElement: <p>the email that you used is not exist</p>,
+                                errorTitle: 'Email not exist'
+                            });
+                            break;
+                        case 1:
+                            // show admin panel
+                            // props.setUserAction(myState.email)
+                            history.push('/user');
+                            // console.log('ok ok ok');
+                            break;
+                        default:
+                            break;
+                    }
+                })
+                .catch(error => {
+                    setMyState({
+                        ...myState,
+                        entriesError: true,
+                        errorElement: <p>can not send the data</p>,
+                        errorTitle: 'unknown error'
+                    });
+                });
         }
-      }).catch(error => {
-        setMyState({...myState, entriesError: true, errorElement: <p>can not send the data</p>, errorTitle: 'unknown error' });
-      })
-    }
-  };
-  const closeModal = () => {
-    setMyState({
-      ...myState,
-      entriesError: false
-    })
-  };
+    };
+    const closeModal = () => {
+        setMyState({
+            ...myState,
+            entriesError: false
+        });
+    };
 
-  //console.log(myState);
+    //console.log(myState);
 
-  return (
-    <React.Fragment>
-      <PopUpModal
-        show={myState.entriesError}
-        close={closeModal}
-        className="bg-danger"
-        title={myState.errorTitle}>
-          {myState.errorElement}
-        </PopUpModal>
-      <Container>
-        <div className="breadcrumb">
+    return (
+        <React.Fragment>
+            <PopUpModal show={myState.entriesError} close={closeModal} className="bg-danger" title={myState.errorTitle}>
+                {myState.errorElement}
+            </PopUpModal>
+            <Container className="mt-5 pt-5">
+                {/* <div className="breadcrumb">
           <div className="container">
             <Link className="breadcrumb-item" to="/">Home</Link>
             <span className="breadcrumb-item active">Login</span>
           </div>
-        </div>
-          <h1>My Account / login</h1>
-          <p>
-            login to start your devices management
-          </p>
-          <InputGroup className="mb-3">
-              <div className="row">
-                <div className="col-md-4">
-                  <Input
-                    type="email"
-                    placeholder="Enter User Name"
-                    required
-                    onChange={(e) => {
-                    setMyState({
-                      ...myState,
-                      email: e.target.value
-                    })
-                  }}
-                    value={myState.email}/>
-                  <span className="required-star">*</span>
-                </div>
-                <div className="col-md-4">
-                  <Input
-                    type="password"
-                    placeholder="Password"
-                    required
-                    onChange={(e) => {
-                    setMyState({
-                      ...myState,
-                      password: e.target.value
-                    })
-                  }}
-                    value={myState.password}/>
-                  {/* <span className="required-star">*</span> */}
-                </div>
-
-                <div className="col-md-4">
-                  <Button className="btn black" onClick={onLoginBtnClick}>Login</Button>
-                </div>
-                <div className="col-md-4">
-                  <h5>Not Registered?
-                    <Link to="/register"> register here</Link>
-                  </h5>
-                </div>  <div className="col-md-4">
-                  <h5>Forget Your Password?
-                    <Link to="/resetPass"> Click here..</Link>
-                  </h5>
-                </div>
-              </div>
-          </InputGroup>
-      </Container>
-    </React.Fragment>
-  )
-}
+        </div> */}
+                <h1 className="text-trans mb-4">My Account / login</h1>
+                <p className="text-trans mb-4">login to start your devices management</p>
+                <Form className="text-center pb-md-0 pb-5">
+                    <Row xs="1" sm="1">
+                        <Col>
+                            <FormGroup className="mb-md-4 mb-3 text-left">
+                                <Label className="w-100 h5 text-trans mb-2 ml-2">First Name:</Label>
+                                <Input
+                                    className="badge-pill bg-transparent"
+                                    type="email"
+                                    placeholder="Enter User Name"
+                                    required
+                                    onChange={e => {
+                                        setMyState({
+                                            ...myState,
+                                            email: e.target.value
+                                        });
+                                    }}
+                                    value={myState.email}
+                                />
+                            </FormGroup>
+                        </Col>
+                        <Col>
+                            <FormGroup className="mb-4 text-left">
+                                <Label className="w-100 h5 text-trans mb-2 ml-2">Password:</Label>
+                                <Input
+                                    className="badge-pill bg-transparent"
+                                    className="badge-pill bg-transparent"
+                                    type="password"
+                                    placeholder="Password"
+                                    required
+                                    onChange={e => {
+                                        setMyState({
+                                            ...myState,
+                                            password: e.target.value
+                                        });
+                                    }}
+                                    value={myState.password}
+                                />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                </Form>
+                <Col  className="text-trans mb-4">
+                    <Button className="badge-pill btn-outline-light bg-transparent mt-4" onClick={onLoginBtnClick}>
+                        Login
+                    </Button>
+                </Col>
+                <Col  className="text-trans mb-4">
+                    <h5>
+                        Not Registered?
+                        <Link to="/register"> register here</Link>
+                    </h5>
+                </Col>
+                <Col  className="text-trans mb-4">
+                    <h5>
+                    Forgot Your <Link to="/resetPass"> Password?</Link>
+                    </h5>
+                </Col>
+            </Container>
+        </React.Fragment>
+    );
+};
 
 // export default connect(null,{setUserAction})(Login)
-export default Login
+export default Login;
--- public/src/components/ResetPage.js
+++ public/src/components/ResetPage.js
@@ -1,156 +1,160 @@
 import React, {useState} from 'react';
-import {
-  InputGroup,
-  Button,
-  Input,
-  Container
-} from 'reactstrap';
+import {Container, Row, Col, Form, FormGroup, Label, Input, Button} from 'reactstrap';
 import {useParams, Link} from 'react-router-dom';
 import PopUpModal from './PopUpModal';
 import {resetPass} from '../services/api';
 
 const ResetPage = () => {
-  const params = useParams()
+    const params = useParams();
 
-  console.log(params.email);
-  console.log(params.id /135531);
-  
-const initialState = {
-  password: '',
-  repassword: '',
-  entriesError: false,
-  errorElement: null,
-  errorTitle: ''
-}
-const [myState, setMyState] = useState(initialState);
+    console.log(params.email);
+    console.log(params.id / 135531);
 
-const onConfirmBtnClick = (e) => {
-  e.preventDefault()
-  if (myState.password !== myState.repassword || myState.password.trim() === '') {
-    const errorElement = (
-      <ul> 
-        {myState
-        .password
-        .trim() === ''
-        ? <li>Password is empty <br></br> please make sure that you use a valid Password</li>
-        : null}
-        {myState.password !== myState.repassword ? <li>Password didn't match Repassword, <br></br> Please Be Sure That Password and Repassword is matching each other</li>: null}
-      </ul>
-    )
-    setMyState({
-      ...myState,
-      entriesError: true,
-      errorElement,
-      errorTitle: 'Entries Error'
-    })
-  } else {
-      resetPass(params.email, params.id, myState.password).then(data => {
-      switch (data) {
-        case 2:
-          setMyState({...myState, entriesError: true, errorElement: <p>there was a server error</p>, errorTitle: 'Server Error' });
-          break;
-        case 4:
-          setMyState({...myState, entriesError: true, errorElement: <p>the email that you used is not exist</p>, errorTitle: 'Email not exist' });
-          break;
-        case 1:
-          setMyState({...myState, entriesError: true, errorElement: <p>We have reset your password, <br></br> tray to login from  <Link className="breadcrumb-item" to="/login"> here </Link>  </p>, errorTitle: 'Password is successfully changed' });
-          console.log('ok ok ok');
-          break;
-      
-        default:
-          break;
-      }
-    }).catch(error => {
-      setMyState({...myState, entriesError: true, errorElement: <p>can not send the data</p>, errorTitle: 'unknown error' });
-    })
-  }
-};
-const closeModal = () => {
-  setMyState({
-    ...myState,
-    entriesError: false
-  })
-};
+    const initialState = {
+        password: '',
+        repassword: '',
+        entriesError: false,
+        errorElement: null,
+        errorTitle: ''
+    };
+    const [myState, setMyState] = useState(initialState);
 
-  
-  return (
-    <React.Fragment>
-      <PopUpModal
-        show={myState.entriesError}
-        close={closeModal}
-        className="bg-danger"
-        title={myState.errorTitle}>
-          {myState.errorElement}
-        </PopUpModal>
-      <Container>
-          <h1>My Account /Reset Password</h1> <br/>
-          <h3>Please Enter The New Password</h3> <br/>
-          <InputGroup className="mb-3">
-                <div className="col-md-6">
-                  <Input
-                    type="password"
-                    placeholder="Enter New Password"
-                    required
-                    onChange={(e) => {
+    const onConfirmBtnClick = e => {
+        e.preventDefault();
+        if (myState.password !== myState.repassword || myState.password.trim() === '') {
+            const errorElement = (
+                <ul>
+                    {myState.password.trim() === '' ? (
+                        <li>
+                            Password is empty <br></br> please make sure that you use a valid Password
+                        </li>
+                    ) : null}
+                    {myState.password !== myState.repassword ? (
+                        <li>
+                            Password didn't match Repassword, <br></br> Please Be Sure That Password and Repassword is
+                            matching each other
+                        </li>
+                    ) : null}
+                </ul>
+            );
+            setMyState({
+                ...myState,
+                entriesError: true,
+                errorElement,
+                errorTitle: 'Entries Error'
+            });
+        } else {
+            resetPass(params.email, params.id, myState.password)
+                .then(data => {
+                    switch (data) {
+                        case 2:
+                            setMyState({
+                                ...myState,
+                                entriesError: true,
+                                errorElement: <p>there was a server error</p>,
+                                errorTitle: 'Server Error'
+                            });
+                            break;
+                        case 4:
+                            setMyState({
+                                ...myState,
+                                entriesError: true,
+                                errorElement: <p>the email that you used is not exist</p>,
+                                errorTitle: 'Email not exist'
+                            });
+                            break;
+                        case 1:
+                            setMyState({
+                                ...myState,
+                                entriesError: true,
+                                errorElement: (
+                                    <p> 
+                                        We have reset your password, <br></br> tray to login from{' '}
+                                        <Link className="breadcrumb-item" to="/login">
+                                            {' '}
+                                            here{' '}
+                                        </Link>{' '}
+                                    </p>
+                                ),
+                                errorTitle: 'Password is successfully changed'
+                            });
+                            console.log('ok ok ok');
+                            break;
+
+                        default:
+                            break;
+                    }
+                })
+                .catch(error => {
                     setMyState({
-                      ...myState,
-                      password: e.target.value
-                    })
-                  }}
-                    value={myState.password}/>
-                  <span className="required-star">*</span>
-                </div>
-                <div className="col-md-6">
-                  <Input
-                    type="password"
-                    placeholder="Repeat Password"
-                    required
-                    onChange={(e) => {
-                    setMyState({
-                      ...myState,
-                      repassword: e.target.value
-                    })
-                  }}
-                    value={myState.repassword}/>
-                  <span className="required-star">*</span>
-                </div>
-                <div className="col-md-4">
-                  <Button className="btn black" onClick={onConfirmBtnClick}>Confirm</Button>
-                </div>
-          </InputGroup>
-      </Container>
-    </React.Fragment>
-  )
-}
+                        ...myState,
+                        entriesError: true,
+                        errorElement: <p>can not send the data</p>,
+                        errorTitle: 'unknown error'
+                    });
+                });
+        }
+    };
+    const closeModal = () => {
+        setMyState({
+            ...myState,
+            entriesError: false
+        });
+    };
 
-export default ResetPage
+    return (
+        <React.Fragment>
+            <PopUpModal show={myState.entriesError} close={closeModal} className="bg-danger" title={myState.errorTitle}>
+                {myState.errorElement}
+            </PopUpModal>
+            <Container className="mt-5 pt-5">
+                <h1 className="text-trans mb-4">My Account /Reset Password</h1> <br />
+                <h3 className="text-trans mb-4">Please Enter The New Password</h3> <br />
+                <Form className="text-center pb-md-0 pb-5">
+                <Col>
+                        <Input
+                            className="badge-pill bg-transparent"
+                            type="password"
+                            placeholder="Enter New Password"
+                            required
+                            onChange={e => {
+                                setMyState({
+                                    ...myState,
+                                    password: e.target.value
+                                });
+                            }}
+                            value={myState.password}
+                        />
+                        <span className="required-star">*</span>
+                        </Col>
+                        <Col>
+                        <Input
+                            className="badge-pill bg-transparent"
+                            type="password"
+                            placeholder="Repeat Password"
+                            required
+                            onChange={e => {
+                                setMyState({
+                                    ...myState,
+                                    repassword: e.target.value
+                                });
+                            }}
+                            value={myState.repassword}
+                        />
+                        <span className="required-star">*</span>
+                        </Col>
+                        <Col>
+                        <Button className="badge-pill btn-outline-light bg-transparent mt-8" onClick={onConfirmBtnClick}>
+                            Confirm
+                        </Button>
+                        </Col>
+                </Form>
+            </Container>
+        </React.Fragment>
+    );
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default ResetPage;
 
 // import React, {useEffect, useState} from 'react';
 // import {useParams, Link, useHistory} from 'react-router-dom';
