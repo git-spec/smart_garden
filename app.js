2f88a2dceeada071bf443323215efe4e4cfb0fc1
felixwurst <felix.wurst@gmail.com>
2020-10-28 00:46:39 +0100

    added check connection for devices

--- app.js
+++ app.js
@@ -2,25 +2,47 @@ const socket = require('socket.io-client')('http://felix.local:5000');
 const {log} = require('console');
 const Transmitter = require('./modules/transmitter')
 
-const device_info = {
-    sn_number: 'aNLWTgH&$^k_L2jN'
+const hub_info = {
+    sn_number: '0xABCDABCD71'
 };
+const devices = [];
 
 socket.on('connect', () => {
     log('connected');
-    socket.emit('device_connect', device_info);
+    socket.emit('hub_device_connect', hub_info); // hub_connect?
 });
 
-socket.on('toDevice', message => {
+socket.on('toDevice', message => { // devices instead of message
     log(message);
+    // save these devices to somewhere... ex: db, file?
 });
 
 const radio = new Transmitter();
 
 radio.setReadingPipe('0xABCDABCD71');
 
-radio.begin();
-
 radio.read(data => {
     log(data);
 });
+
+// radio.send('data from rasbperry', 10, '0x744d52687C').then(() => {
+//     console.log('sent');
+// }).catch(() => {
+//     console.log('error on sending');
+// });
+
+// checks every 20 s if a device is connected 
+setInterval(() => {
+    checkConnected();
+}, 20 * 1000);
+
+// for each device related to this hub
+function checkConnected() {
+    radio.send('hi', 10, '0x744d52687C').then(() => {
+        console.log('connected');
+        socket.emit('device_connect', '0x744d52687C')
+    }).catch(() => {
+        console.log('disconnected');
+        socket.emit('device_disconnect', '0x744d52687C')
+    });
+}
