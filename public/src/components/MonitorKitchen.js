259dbac402d0f8b0b368678c8f63bcacbb796e44
felixwurst <felix.wurst@gmail.com>
2020-11-18 21:05:39 +0100

    added monitors for hubs

---
+++ public/src/components/MonitorKitchen.js
@@ -0,0 +1,36 @@
+// react
+import React, {Fragment} from 'react';
+// reactstrap
+import {Table} from 'reactstrap';
+
+
+const MonitorKitchen = props => {
+
+    return (
+        <Fragment>
+            <h3 className="text-center">kitchen</h3>
+            <Table style={{tableLayout: "fixed"}} borderless size="sm" className="mb-4">
+                <thead>
+                    <tr>
+                        <th className="align-bottom text-uppercase">Devices</th>
+                        <th><i className="fas fa-thermometer-half mr-2 mb-2"></i></th>
+                        <th><i className="fas fa-cloud mr-2 mb-2"></i></th>
+                        <th><i className="fas fa-tint mr-2 mb-2"></i></th>
+                        <th><i className="fas fa-lightbulb mr-2 mb-2"></i></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">herbs</th>
+                        <td></td>
+                        <td></td>
+                        <td>51%</td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </Table>
+        </Fragment>
+    )    
+}
+
+export default MonitorKitchen;
