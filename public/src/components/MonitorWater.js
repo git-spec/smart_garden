638297a795521987e1ce3522579d7658f5f7c43a
felixwurst <felix.wurst@gmail.com>
2020-11-18 21:09:45 +0100

    added device selector and save button

--- public/src/components/MonitorWater.js
+++ public/src/components/MonitorWater.js
@@ -1,42 +1,65 @@
 // react
-import React, {Fragment, Component} from 'react';
+import React, { Fragment, Component } from 'react';
 // reactstrap
 import {
     Input,
     Label,
-    Table
+    Table,
+    Button,
+    FormGroup,
+    CustomInput
 } from 'reactstrap';
-// components
-// import LineChart from './LineChart';
-// import LineChartMultiple from './LineChartMultiple';
-// import BarChartHorizontal from './BarChartHorizontal';
-// services
-import {getData} from '../services/getData';
 
 
-class MonitorSoil extends Component {
-
-    data = getData();
-    // console.log('data: ', this.props.data);
+class MonitorWater extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            inputRangeTime: 0,
-            inputRangeDuration: 0,
+            inputRangeTime: this.props.device.water_time,
+            inputRangeDuration: this.props.device.water_duration,
+            soilMoistureDevice: this.props.device.moisture_device_id,
+            deviceID: null
             // inputRangeMax: "100",
             // graphHeightMax: 160,
             // graphHeightMin: 20
         };
         this.rangeStatusMinRef = React.createRef();
-        this.rangeStatusMaxRef = React.createRef();    
+        this.rangeStatusMaxRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.rangeStatusMinRef.current.style.setProperty('--thumb-input', this.state.inputRangeTime);
+        this.rangeStatusMinRef.current.style.setProperty('--output-width', this.rangeStatusMinRef.current.offsetWidth + "px");
+        this.rangeStatusMaxRef.current.style.setProperty('--thumb-input', this.state.inputRangeDuration);
+        this.rangeStatusMaxRef.current.style.setProperty('--output-width', this.rangeStatusMaxRef.current.offsetWidth + "px");
+        this.setState({
+            deviceID: this.props.device.id,
+            inputRangeTime: this.props.device.water_time,
+            inputRangeDuration: this.props.device.water_duration,
+            soilMoistureDevice: this.props.device.moisture_device_id,
+        });     
+    }
+    componentDidUpdate() {
+        if(this.props.device.id !== this.state.deviceID) {
+            this.rangeStatusMinRef.current.style.setProperty('--thumb-input', this.props.device.water_time);
+            this.rangeStatusMinRef.current.style.setProperty('--output-width', this.rangeStatusMinRef.current.offsetWidth + "px");
+            this.rangeStatusMaxRef.current.style.setProperty('--thumb-input', this.props.device.water_duration);
+            this.rangeStatusMaxRef.current.style.setProperty('--output-width', this.rangeStatusMaxRef.current.offsetWidth + "px");  
+            this.setState({
+                deviceID: this.props.device.id,
+                inputRangeTime: this.props.device.water_time,
+                inputRangeDuration: this.props.device.water_duration,
+                soilMoistureDevice: this.props.device.moisture_device_id,
+            }); 
+        }
     }
 
     onBtnInputRangeTime = (e, output) => {
         e.preventDefault();
         // output of current value
         output.current.innerText = e.target.value;
-        this.setState({inputRangeTime: e.target.value});
+        this.setState({ inputRangeTime: e.target.value });
         // send current properties to css
         output.current.style.setProperty('--thumb-input', e.target.value);
         output.current.style.setProperty('--output-width', output.current.offsetWidth + "px");
@@ -45,43 +68,61 @@ class MonitorSoil extends Component {
         e.preventDefault();
         // output of current value
         output.current.innerText = e.target.value;
-        this.setState({inputRangeDuration: e.target.value});
+        this.setState({ inputRangeDuration: e.target.value });
         // send current properties to css
         output.current.style.setProperty('--thumb-input', e.target.value);
         output.current.style.setProperty('--output-width', output.current.offsetWidth + "px");
     }
+    onSoilMoistureSelect = e => {
+        e.preventDefault();
+        this.setState({soilMoistureDevice: e.target.value});
+    }
 
     render() {
+        let options = null;
+        if(this.props.devices){
+            const soilMoistureDevices = this.props.devices.filter(device => device.hub_id === this.props.device.hub_id && device.type_id === 1)
+             options = soilMoistureDevices.map(device => {
+                return (
+                    <option value={device.id} key={device.id}>{device.name}</option>
+                )
+            })
+        }
         return (
             <Fragment>
-                {/* <h3 className="text-center">{this.props.title}</h3> */}
+                <h3 className="text-center">{this.props.hub.name}</h3>
                 <Table borderless size="sm" className="mb-5">
                     <thead>
-                        <tr>
-                            <th className="align-bottom"><h3 className="mb-0">{this.props.hub}</h3></th>
-                            <th><i className="fas fa-tint mr-2 mb-2"></i></th>
+                        <tr>                            
+                            <th className="align-bottom text-uppercase">{this.props.device.name}</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <th scope="row">{this.props.device}</th>
-                            <td>???%</td>
-                            {/* <td>{this.props.data[0]}%</td> */}
+                            <th scope="row">{this.props.device.device_name}</th>
                         </tr>
                     </tbody>
                 </Table>
                 {/* on off button */}
                 <div className="d-flex align-items-center">
                     <label className="switch">
-                        <input type="checkbox" checked={this.props.status} onChange={this.props.statusChange} />
+                        <input type="checkbox" checked={this.props.device.status} onChange={this.props.statusChange} />
                         <span className="slider round"></span>
                     </label>
-                    <span className="ml-3">waterpump: {this.props.status ? 'ON' : 'OFF'}</span>
+                    <span className="ml-3">waterpump: {this.props.device.status ? 'ON' : 'OFF'}</span>
                 </div>
-                {/* range 1 */}
+                {/* soil moisture selector */}
+                <FormGroup>
+                    <Label for="soilMoistureSelect">soil moisture modules:</Label>
+                    <CustomInput type="select" id="soilMoistureSelect" name="soilMoistureSelect" onChange={this.onSoilMoistureSelect} value={this.state.soilMoistureDevice ? this.state.soilMoistureDevice : ''}>
+                        <option value="">select your module</option>
+                        {options}
+                    </CustomInput>
+                </FormGroup>
+                {/* range time */}
                 <div className="range mt-2 min">
-                <Label for="rangeInput">time of watering: soil moisture &#60; {this.state.inputRangeTime}%</Label>
-                    <output ref={this.rangeStatusMinRef} name="amount" id="amount" htmlFor="rangeInput">0</output>
+                    <Label for="rangeInput">time of watering: soil moisture &#60; {this.state.inputRangeTime}%</Label>
+                    <output ref={this.rangeStatusMinRef} name="amount" id="amount" htmlFor="rangeInput">{this.state.inputRangeTime}</output>
                     <div>
                         <Input
                             type="range"
@@ -89,19 +130,16 @@ class MonitorSoil extends Component {
                             name="rangeInput"
                             min="0"
                             max="100"
-                            defaultValue="0"
-                            // onInput= {function (e) {e.preventDefault()
-                            //     this.output.amount.value=this.value}}
-                            // onInput={amount.value = parseInt(this.value)}
-                            onInput={e => this.onBtnInputRangeTime(e, this.rangeStatusMinRef)}
+                            value={this.state.inputRangeTime}
+                            onChange={e => this.onBtnInputRangeTime(e, this.rangeStatusMinRef)}
                         />
                     </div>
                     <output>100</output>
                 </div>
-                {/* range 2 */}
+                {/* range duration */}
                 <div className="range mt-2 max">
                     <Label for="rangeInput">duration of watering: {this.state.inputRangeDuration}s</Label>
-                    <output ref={this.rangeStatusMaxRef} name="amount" id="amount" htmlFor="rangeInput">0</output>
+                    <output ref={this.rangeStatusMaxRef} name="amount" id="amount" htmlFor="rangeInput">{this.state.inputRangeDuration}</output>
                     <div>
                         <Input
                             type="range"
@@ -109,35 +147,22 @@ class MonitorSoil extends Component {
                             name="rangeInput"
                             min="0"
                             max="100"
-                            defaultValue="0"
-                            // onInput= {function (e) {e.preventDefault()
-                            //     this.output.amount.value=this.value}}
-                            // onInput={amount.value = parseInt(this.value)}
-                            onInput={e => this.onBtnInputRangeDuration(e, this.rangeStatusMaxRef)}
+                            value={this.state.inputRangeDuration}
+                            onChange={e => this.onBtnInputRangeDuration(e, this.rangeStatusMaxRef)}
                         />
                     </div>
                     <output>100</output>
                 </div>
-                {/* <LineChart data={this.data[0].data} title={this.data[0].title} color="rgb(0, 168, 230)" min={this.props.min} max={this.props.max} /> */}
-                {/* <LineChartMultiple data={data[0].data} title={data[0].title} color="rgb(0, 168, 230)" /> */}
-                {/* <BarChartHorizontal data={data[3].data} title={data[3].title} color="rgb(0, 168, 230)" /> */}
-                {/* <FormGroup>
-                    <Label for="rangeInput">Range</Label>
-                    <Input
-                        type="range"
-                        id="rangeInput"
-                        name="rangeInput"
-                        min="0"
-                        max="100"
-                        // onInput="this.output.amount.value=this.value"
-                    />
-                    <output name="amount" id="amount" htmlFor="rangeInput">
-                        0
-                    </output>
-                </FormGroup> */}
+                {/* save button */}
+                <Button
+                    outline color="secondary"
+                    onClick={e => this.props.save(e, this.state.inputRangeTime, this.state.inputRangeDuration, this.state.soilMoistureDevice)}
+                >
+                    save
+                </Button>
             </Fragment>
         )
     }
 };
 
-export default MonitorSoil;
+export default MonitorWater;
