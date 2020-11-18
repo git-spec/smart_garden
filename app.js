febecd642d6dbdaaa1350f1aa5030c7b880e91b1
felixwurst <felix.wurst@gmail.com>
2020-11-18 01:51:05 +0100

    water pump controlling

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ socket.on('connect', () => {
     // saving data to db every 30 minutes
     setInterval(() => {
         requestDataInterval();
-    }, 30 * 60 * 1000); 
+    }, 30 * 60 * 1000);
 });
 
 // list of all devices in the cloud
@@ -57,39 +57,66 @@ socket.on('user_disconnect', () => {
 // incoming order for realtime data
 socket.on('realTimeRequest', sn => {
     // change focus to true
-    devices[devices.map(device => device.sn_number).indexOf(sn)].focus = true;
-    // realtime data request for all devices except waterpump
-    if (devices[devices.map(device => device.sn_number).indexOf(sn)].type_id != 2) {
-        radio.send('realTimeData', 10, sn).then(() => {
-            log(`realtime data request sent to "${sn}"`);
-        }).catch(error => {
-            log(error);
-        });    
+    const device = devices[devices.map(device => device.sn_number).indexOf(sn)]
+    if(device){
+        devices[devices.map(device => device.sn_number).indexOf(sn)].focus = true;
+        // realtime data request for all devices except waterpump
+        if (devices[devices.map(device => device.sn_number).indexOf(sn)].type_id != 2) {
+            radio.send('realTimeData', 10, sn).then(() => {
+                log(`realtime data request sent to "${sn}"`);
+            }).catch(error => {
+                log(error);
+            });
+        }
     }
 });
 // stop real time data for some device
 socket.on('stopRealTimeData', sn => {
     // change focus to false
-    devices[devices.map(device => device.sn_number).indexOf(sn)].focus = false;
-    radio.send('stopRealTimeData', 10, sn).then(() => {
-        log(`stop realtime data request for device "${sn}"`);
-    }).catch(error => {
-        log(error);
-    })
-})
+    const device = devices[devices.map(device => device.sn_number).indexOf(sn)]
+    if(device){
+        devices[devices.map(device => device.sn_number).indexOf(sn)].focus = false;
+        radio.send('stopRealTimeData', 10, sn).then(() => {
+            log(`stop realtime data request for device "${sn}"`);
+        }).catch(error => {
+            log(error);
+        });
+    }
+});
 
 // get status from water switcher
 socket.on('waterOnOff', data => {
     // switch status from device inside devices
-    devices[devices.map(device => device.sn_number).indexOf(data.sn)].status = data.status;
-    const message = data.status ? 'on' : 'off';
-    // send message on or off to arduino
-    radio.send(message, 10, data.sn).then(() => {
-        log(`status for water device ${data.sn}: "${data.status}"`);
-    }).catch(error => {
-        log(error);
-    });
+    const device = devices[devices.map(device => device.sn_number).indexOf(data.sn)]
+    if(device){
+        devices[devices.map(device => device.sn_number).indexOf(data.sn)].status = data.status;
+        const message = data.status ? 'on' : 'off';
+        // send message on or off to arduino
+        radio.send(message, 10, data.sn).then(() => {
+            log(`status for water device ${data.sn}: "${data.status}"`);
+        }).catch(error => {
+            log(error);
+        });
+    }
 });
+
+socket.on('waterConf', data => {
+    // switch status from device inside devices
+    const device = devices[devices.map(device => device.sn_number).indexOf(data.sn)]
+    if(device){
+        devices[devices.map(device => device.sn_number).indexOf(data.sn)].water_time = data.time;
+        devices[devices.map(device => device.sn_number).indexOf(data.sn)].water_duration = data.duration;
+        devices[devices.map(device => device.sn_number).indexOf(data.sn)].moisture_device_id= data.soilMoistureDevice;
+        const message = data.status ? 'on' : 'off';
+        // send message on or off to arduino
+        radio.send('conf-' + (data.duration ? data.duration : '5'), 3, device.sn_number).then(() => {
+
+        }).catch(err => {
+            
+        })
+    }
+})
+
 
 
 // read the data getting from device
@@ -98,26 +125,51 @@ radio.read(data => {
     const message = data.substr(data.indexOf('-') + 1, data.length);
     const device = devices.find(device => device.sn_number === sn);
     if (device && message) {
-        if (message.toString().replace(/\x00/gi, '') === 'yup' || message.toString().replace(/\x00/gi, '') === 'hi') { 
-            // device is connected 
+        if (message.toString().replace(/\x00/gi, '') === 'yup' || message.toString().replace(/\x00/gi, '') === 'hi') {
+            // device is connected
             let dev = devices.find(item => item.sn_number === device.sn_number && !item.connected);
             if (dev) {
+                if (dev.type_id === 2) {
+                    radio.send('conf-' + (dev.water_duration ? dev.water_duration : '5'), 3, dev.sn_number).then(() => {
+
+                    }).catch(err => {
+                        
+                    })
+                }
                 // set the device from which the data was received to connected
                 dev.connected = 1;
                 devices[devices.map(device => device.id).indexOf(device.id)] = dev;
                 log(`Device "${device.name}" is connected now`);
                 socket.emit('device_connect', device.sn_number);
             }
-        } else { 
+        } else {
             // getting sensor data from device
             log(`Message from "${sn}": ${message}`);
             let str = message.replace(/\0/g, '');
             let data = [];
-            str.split('|').forEach(d => {
-                data.push(d);
+            str.split('|').forEach(number => {
+                data.push(number);
             });
             let dev = devices.find(item => item.sn_number === device.sn_number && item.requestDataInterval);
-            if (dev) { 
+            if (dev) {
+                if(dev.type_id == 1){
+                    const waterDevices = devices.filter(device => device.moisture_device_id == dev.id)
+                    waterDevices.forEach(device => {
+                        const triggerValue = parseInt(device.water_time)
+                        const currentValue = parseInt(data[0])
+                        if(currentValue < triggerValue) {
+                            radio.send('conf-' + (device.water_duration ? device.water_duration : '5'), 3, device.sn_number).then(() => {
+                                radio.send('on' , 3, device.sn_number).then(() => {
+
+                                }).catch(err => {
+                                    
+                                })
+                            }).catch(err => {
+                                
+                            })
+                        }
+                    })
+                }
                 // sending data to database
                 devices[devices.map(device => device.id).indexOf(dev.id)].requestDataInterval = false;
                 socket.emit('deviceDataInterval', {device: dev.id, data: data});
@@ -129,7 +181,7 @@ radio.read(data => {
                         log(error);
                     })
                 }
-            } else { 
+            } else {
                 // sending realtime data
                 if (device.focus) {
                     socket.emit('realTimeData', {sn_number: sn, data: data});
@@ -149,7 +201,7 @@ function checkAllDevices() {
         checkConnected(device).then(() => {
 
         }).catch(error => {
-            
+
         })
     })
 }
@@ -189,7 +241,7 @@ function requestRealTimeDataFromAllDevices() {
             requestRealTimeData(device).then(() => {
 
             }).catch(error => {
-    
+
             })
         }
     })
@@ -215,8 +267,8 @@ function stopRequestRealTimeDataFromAllDevices() {
             stopRequestRealTimeData(device).then(() => {
 
             }).catch(error => {
-    
-            })    
+
+            })
         }
     })
 }
